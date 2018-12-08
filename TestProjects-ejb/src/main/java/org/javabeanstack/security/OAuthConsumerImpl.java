f7f3df0a6f2c3ab5ef9058a32dc9c56d387e5770
JORGE <jorge.enciso.r@gmail.com>
2018-12-08 09:07:06 -0300

    Preparar clases para acceso a datos via token

---
+++ TestProjects-ejb/src/main/java/org/javabeanstack/security/OAuthConsumerImpl.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javabeanstack.security;
+
+import org.javabeanstack.model.IAppAuthConsumer;
+import org.javabeanstack.model.IAppAuthConsumerToken;
+
+/**
+ *
+ * @author Jorge Enciso
+ */
+public class OAuthConsumerImpl extends OAuthConsumer{
+        @Override
+        public Class<IAppAuthConsumer> getAuthConsumerClass() {
+            try {
+                return (Class<IAppAuthConsumer>)Class.forName("org.javabeanstack.model.appcatalog.AppAuthConsumer");
+            } catch (ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());                
+            }
+            return null;
+        }
+
+        @Override
+        public Class<IAppAuthConsumerToken> getAuthConsumerTokenClass() {
+            try {            
+                return (Class<IAppAuthConsumerToken>)Class.forName("org.javabeanstack.model.appcatalog.AppAuthConsumerToken");
+            } catch (ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            }
+            return null;
+        }    
+}
