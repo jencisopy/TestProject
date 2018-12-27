c13ddd15aa81d6046355fd55677df98bd361b060
JORGE <jorge.enciso.r@gmail.com>
2018-12-27 16:04:56 -0300

    JBSFRAME-28 Webservices adicionales

--- TestProjects-ejb/src/main/java/org/javabeanstack/security/OAuthConsumerImpl.java
+++ TestProjects-ejb/src/main/java/org/javabeanstack/security/OAuthConsumerImpl.java
@@ -5,6 +5,7 @@
  */
 package org.javabeanstack.security;
 
+import org.javabeanstack.data.services.IAppCompanySrv;
 import org.javabeanstack.model.IAppAuthConsumer;
 import org.javabeanstack.model.IAppAuthConsumerToken;
 
@@ -12,24 +13,24 @@ import org.javabeanstack.model.IAppAuthConsumerToken;
  *
  * @author Jorge Enciso
  */
-public class OAuthConsumerImpl extends OAuthConsumerBase{
-        @Override
-        public Class<IAppAuthConsumer> getAuthConsumerClass() {
-            try {
-                return (Class<IAppAuthConsumer>)Class.forName("org.javabeanstack.model.appcatalog.AppAuthConsumer");
-            } catch (ClassNotFoundException ex) {
-                System.out.println(ex.getMessage());                
-            }
-            return null;
+public class OAuthConsumerImpl extends OAuthConsumerBase {
+    @Override
+    public Class<IAppAuthConsumer> getAuthConsumerClass() {
+        try {
+            return (Class<IAppAuthConsumer>) Class.forName("org.javabeanstack.model.appcatalog.AppAuthConsumer");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
         }
+        return null;
+    }
 
-        @Override
-        public Class<IAppAuthConsumerToken> getAuthConsumerTokenClass() {
-            try {            
-                return (Class<IAppAuthConsumerToken>)Class.forName("org.javabeanstack.model.appcatalog.AppAuthConsumerToken");
-            } catch (ClassNotFoundException ex) {
-                System.out.println(ex.getMessage());
-            }
-            return null;
-        }    
+    @Override
+    public Class<IAppAuthConsumerToken> getAuthConsumerTokenClass() {
+        try {
+            return (Class<IAppAuthConsumerToken>) Class.forName("org.javabeanstack.model.appcatalog.AppAuthConsumerToken");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
 }
